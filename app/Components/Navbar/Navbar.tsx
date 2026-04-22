f7cf4d9260294342056c5ab603716745034da296
Jorge Matos <jisapjisap@gmail.com>
2026-04-22 17:06:56 +0200

    feat: initialize project with global styles, custom fonts, and base Navbar component

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,4 +1,19 @@
-import React from 'react'
+"use client"
+
+type NavLink = {
+  label: string;
+  href: string;
+  dropdown?: { label: string; href: string; }
+}
+
+const navLinks: NavLink[] = [
+  { label: "Home", href: "/" },
+  { label: "Men's", href: "/Pages/MensProducts/" },
+  { label: "Women's", href: "/Pages/WomensProduct/" },
+  { label: "Shoes's", href: "/Pages/ShoesProduct/" },
+  { label: "Collections", href: "/Pages/Collections" },
+  { label: "Blogs", href: "/Pages/Blogs" },
+];
 
 const Navbar = () => {
   return (
