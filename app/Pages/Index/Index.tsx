55ab5a7d48a7a71646bcddcadd30a9688cbc238d
Jorge Matos <jisapjisap@gmail.com>
2026-04-24 16:25:32 +0200

    feat: add Banner component and global styles for home page layout

--- app/Pages/Index/Index.tsx
+++ app/Pages/Index/Index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Hero/page'
 import Discover from './Discover/page'
+import Banner from './Banner.tsx/page'
 
 
 const Index = () => {
@@ -8,6 +9,7 @@ const Index = () => {
     <>
       <Hero />
       <Discover />
+      <Banner />
     </>
   )
 }
